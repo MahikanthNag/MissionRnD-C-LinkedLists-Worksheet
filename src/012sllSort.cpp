bd8f52a56bfdd66744490c5c4cd45b7f41ec347d
MahikanthNag <babanag95@gmail.com>
2015-12-30 13:16:04 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -1,5 +1,5 @@
 /*
-OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes 
+OVERVIEW:  Given a single linked list of 0s 1s and 2s ,Sort the SLL such that zeroes
 will be followed by ones and then twos.
 
 
@@ -21,6 +21,37 @@ struct node {
 	struct node *next;
 };
 
-void sll_012_sort(struct node *head){
-	
+void sll_012_sort(struct node *head)
+{
+        node *p=(struct node*)malloc(sizeof(struct node));
+        node *q1=(struct node*)malloc(sizeof(struct node));
+        node *q2=(struct node*)malloc(sizeof(struct node));
+        int i,j,c=1,t;
+        p=head;
+        q1=p;q2=p;
+        while(q1->next!=NULL)
+        {
+            q1=q1->next;c++;
+        }
+
+        for(i=0;i<c;i++)
+        {
+            for(j=0;j<c-i-1;j++)
+            {
+                if(q2->data>q2->next->data)
+                {
+                    t=q2->data;q2->data=q2->next->data;q2->next->data=t;
+                }
+                q2=q2->next;
+            }
+            //q1=q1->next;
+            q2=p;
+        }
+        head=p;
+        q2=p;
+        while(q2->next!=NULL)
+        {
+            printf("***%d",q2->data);q2=q2->next;
+        }
+        printf("***%d",q2->data);
 }
