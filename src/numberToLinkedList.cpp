d057eeddcd92469bd84ea677c83356f821b16c36
MahikanthNag <babanag95@gmail.com>
2015-12-30 12:56:30 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -6,7 +6,7 @@ INPUTS:  A number.
 
 OUTPUT: Create linked list from given number, each digit as a node.
 
-ERROR CASES: 
+ERROR CASES:
 
 NOTES: For negative numbers ignore negative sign.
 */
@@ -19,6 +19,64 @@ struct node {
 	struct node *next;
 };
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+struct node * numberToLinkedList(int N)
+{
+//	return NULL;
+    int rem,c=0;int x;
+    node *p=(struct node*)malloc(sizeof(struct node));//p=NULL;
+    node *q=(struct node*)malloc(sizeof(struct node));//q=NULL;
+
+    //p->next=NULL;
+    if(N==0)
+    {
+        p->next=NULL;p->num=0;
+        return p;
+    }
+    if(N<0)N=-N;
+    while(N>0)
+    {
+        rem=N%10;
+        if(c==0)
+        {
+            //printf("did not enter if:");
+            node *temp=(struct node*)malloc(sizeof(struct node));//temp=NULL;
+            temp->num=rem;temp->next=NULL;
+            p=temp;
+            //printf("p->num= %d",p->num);//scanf("%d",&x);
+            c=1;
+        }
+        else
+        {
+            q=p;
+            //p->num=rem;
+            //printf("step1");
+            /*while(q->next!=NULL)
+            {
+                q=q->next;
+            }
+           // printf("step2 after loop");
+            node *temp=(struct node*)malloc(sizeof(struct node));//temp=NULL;
+            temp->num=rem;temp->next=NULL;
+            q->next=temp;printf("q->num= %d",q->num);   */
+            node *temp=(struct node*)malloc(sizeof(struct node));
+            temp->num=rem;temp->next=p;
+            p=temp;
+
+        }N=N/10;//printf("the value of n is %d",N);
+    }
+    return p;
 }
+/*int main()
+{
+    node *p=(struct node*)malloc(sizeof(struct node));
+    node *q=(struct node*)malloc(sizeof(struct node));p->next=q;p->num=2;q->next=NULL;q->num=3;
+    p=numberToLinkedList(3458);
+    q=p;
+    while(q->next!=NULL)
+    {
+        printf("%d  ",q->num);q=q->next;
+    }
+    //printf("%d",q->num);
+    //printf("loop exited");
+    return 0;
+}*/
