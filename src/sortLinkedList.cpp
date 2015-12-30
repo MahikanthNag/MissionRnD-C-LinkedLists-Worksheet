eea65150526797dabdb573e1321e48aac36a3118
MahikanthNag <babanag95@gmail.com>
2015-12-30 15:09:13 +0530

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -12,12 +12,38 @@ NOTES: Without using extra array or linked list.
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
-struct node * sortLinkedList(struct node *head) {
-	return NULL;
+struct node * sortLinkedList(struct node *head)
+{
+
+	//return NULL;
+	if(head==NULL)
+        return NULL;
+	node *p=(struct node*)malloc(sizeof(struct node));
+	node *q=(struct node*)malloc(sizeof(struct node));
+	node *q1=(struct node*)malloc(sizeof(struct node));
+	p=head;
+	int i,j,t,c=0;
+	q=p;
+	while(q!=NULL)
+    {
+        c++;q=q->next;
+    }
+    q=p;q1=p;
+    for(i=0;i<c;i++)
+    {
+        for(j=0;j<c-i-1;j++)
+        {
+            if(q->num>q->next->num) {t=q->num;q->num=q->next->num;q->next->num=t;}
+            q=q->next;
+        }
+        q=p;
+    }
+    return p;
+
 }
